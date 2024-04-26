d8b898b404b59d79a2941e52d752c37811015e0b
Manuchehr <manuchekhr.3232@gmail.com>
2024-04-26 23:45:38 +0500

    feat: setup main configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().port().default(9000),
+        CORS: Joi.string().default('*'),
+        SWAGGER_ENABLED: Joi.boolean().default(false),
+      }),
+      validationOptions: {
+        abortEarly: true,
+      },
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsConfig } from './global/config/cors.config';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerConfig } from './global/config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  new CorsConfig(app).enable(process.env.CORS);
+  new SwaggerConfig(app).enable(process.env.SWAGGER_ENABLED === 'true');
+  await app.listen(+process.env.PORT);
 }
 bootstrap();
