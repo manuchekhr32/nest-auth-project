d53914b7c93db1a34d145499932126cca53de4af
Manuchehr <manuchekhr.3232@gmail.com>
2024-04-27 00:38:42 +0500

    feat: add prisma & docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,13 @@ import * as Joi from 'joi';
         PORT: Joi.number().port().default(9000),
         CORS: Joi.string().default('*'),
         SWAGGER_ENABLED: Joi.boolean().default(false),
+        // DB
+        DB_USER: Joi.string(),
+        DB_PASSWORD: Joi.string(),
+        DB_NAME: Joi.string(),
+        DB_PORT: Joi.number(),
+        DB_HOST: Joi.string(),
+        DB_URL: Joi.string(),
       }),
       validationOptions: {
         abortEarly: true,
